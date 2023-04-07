983c511efcb4e32d9391866aee0c4b9bbf76bdca
zemaciel <>
2023-04-07 15:25:56 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const API_KEY = "Biy70Bb-W5t23Z6FHC5IE6E6uQI";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        // console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.exiry}</div>`;
+    
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results; 
+
+    resultsModal.show();
+}
