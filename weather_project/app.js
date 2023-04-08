ba8e12ba0d9820341ff0046157d46e37b6eded3f
zemaciel <>
2023-04-08 12:54:52 +0000

    give up weather project on this server

--- weather_project/app.js
+++ weather_project/app.js
@@ -1,9 +1,36 @@
-const express = requeire("express");
+const express = require("express");
+const https = require("https");
 
 const app = express();
 
+app.get("/", function (req, res) {
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=02fddf1243fe2697a2246315f46d5be8&units=metric"
+
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data)) {
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.wheather[0].description
+            const icon = weatherData.wheather[0].icon
+            const imageURL = "http://openweathermap.org//img/wn/" + icon + "@2x.png"
+            res.write("The weathr is currently " + weatherDescription + "<p>")
+            res.send("<h1>The temperature in Dublin is " + temp + "degrees Celcius.</h1>");
+            res.write("<img scr=" + imageURL + ">")
+            res.send();
+        });
+
+    res.send("Server is running");
+});
 
 
-app.listen(3000, function() {
-    console.log("server is running on port 3000.")
-})
+
+
+// ------FOR LOCAL HOST
+
+
+// app.listen(3000, function() {
+//     console.log("server is running on port 3000.")
+// })
