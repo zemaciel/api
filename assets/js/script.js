245d525a635340f5fb6d8620920b8f4f9bbd9668
zemaciel <>
2023-04-08 08:47:24 +0000

    Problems with codeanywhere

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,30 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        };
+        body: form,
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    } else {
+        throw new Error(data.error);
+    }
+    }
+
+}
+
+
 async function getStatus(e) {
 
     const queryString = `${API_URL}?api_key=${API_KEY}`;
