78116d0130afa09dec94353c7cf6c8f946b88ec6
zemaciel <>
2023-04-08 09:50:08 +0000

    Finish Codealong Challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        dislpayException(data);
         throw new Error(data.error);
     }
 
@@ -54,6 +55,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        dislpayException(data);
         throw new Error(data.error);
     }
 
@@ -88,6 +90,21 @@ function displayStatus(data) {
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
+}
+
+function dislpayException(data) {
+
+    let heading = `An Exception Occurred`
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong> ${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
     resultsModal.show();
 
 }
